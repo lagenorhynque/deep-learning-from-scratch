e1a01a53cbda0ab37a98536de56852d43e4f8620
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-01-12 17:32:45 +0900

    Implement step function

---
+++ src/deep_learning_from_scratch/ch03/step_function.clj
@@ -0,0 +1,13 @@
+(ns deep-learning-from-scratch.ch03.step-function
+  (:require [clojure.core.matrix :as m]
+            [incanter [charts :as charts] [core :as incanter]]))
+
+(defn step-function [x]
+  (m/gt x 0))
+
+(defn -main []
+  (m/set-current-implementation :clatrix)
+  (let [x (m/array (range -5.0 5.0 0.1))
+        y (step-function x)]
+    (doto (charts/xy-plot x y)
+      incanter/view)))
